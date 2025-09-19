365aa6c5da70fc7835596c76de330fb54c978456
EduardoMG12 <charles.eduardo.mello.guimaraes@hotmail.com>
2025-09-19 20:36:04 -0300

    chore: implement swagger doc

--- go-api/cmd/main.go
+++ go-api/cmd/main.go
@@ -7,9 +7,19 @@ import (
 	"payment-gateway/go-api/internal/config"
 	"payment-gateway/go-api/internal/database"
 
+	httpSwagger "github.com/swaggo/http-swagger"
+
+	_ "payment-gateway/go-api/docs"
+
 	_ "github.com/lib/pq"
 )
 
+// @title Payment Gateway API
+// @version 1.0
+// @description This is a main payment-gateway API .
+// @host localhost:8080
+// @BasePath /
+// @schemes http
 func main() {
 
 	cfg := config.LoadConfig()
@@ -26,6 +36,10 @@ func main() {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
+	http.Handle("/swagger/", httpSwagger.WrapHandler)
+
 	fmt.Println("Server up\nPORT:8080\nhttp://localhost:8080")
+	fmt.Printf("API Swagger doc up: http://localhost:8080/swagger")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
